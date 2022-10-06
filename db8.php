96c976a7c979e83777396b1bff766b0d8dac1e0c
NapassornSt <91865831+NapassornSt@users.noreply.github.com>
2022-10-07 00:11:19 +0700

    Add files via upload

---
+++ db8.php
@@ -0,0 +1,16 @@
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
+
+	<form action="insert_8.php" method="post">
+	Username: <input type="text" name="username"><br><br><br>
+	Password: <input type="Password" name="password"><br><br>
+	Name: <input type="text" name="name"><br><br>
+	Address: <br><br>
+	<textarea name="address" rows="3" cols="40"></textarea><br><br>
+	Tel.: <input type="Tel" name="mobile"><br><br>
+	Email: <input type="text" name="email"><br><br>
+	<input type="submit" value="สมัครสมาชิก ">
+	</form>
+</body>
+</html>
